2948b408fe34d6a2800c8eb8b007ba807d05c9a2
Souvik313 <roysouvik8220@gmail.com>
2026-07-15 17:57:57 +0530

    Change 8

--- backend/routes/resume_routes.py
+++ backend/routes/resume_routes.py
@@ -1,17 +1,45 @@
 from pathlib import Path
 import shutil
 
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter
+from fastapi import UploadFile
+from fastapi import File
+from fastapi import HTTPException
 
-# COMMENT THIS OUT
-# from backend.services.career_pipeline import CareerPipeline
+from backend.services.career_pipeline import CareerPipeline
 
 router = APIRouter()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 UPLOAD_FOLDER = BASE_DIR / "uploads"
 UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
 
 @router.post("/upload-resume")
-async def upload_resume():
-    return {"message": "Route works"}
+async def upload_resume(file: UploadFile = File(...)):
+
+    try:
+        if file.content_type != "application/pdf":
+            raise HTTPException(
+                status_code=400,
+                detail="Only pdf resumes are supported"
+            )
+        
+        file_path = UPLOAD_FOLDER / file.filename
+
+        with open(file_path , "wb") as buffer:
+            shutil.copyfileobj(
+                file.file,
+                buffer
+            )
+
+        pipeline = CareerPipeline()
+        result = pipeline.run_pipeline(
+            str(file_path)
+        )
+        return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e),
+        )
