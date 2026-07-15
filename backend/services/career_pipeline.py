74f8febc5ce615ab6ba9688f6a5e13c8fd413779
Souvik313 <roysouvik8220@gmail.com>
2026-07-15 16:51:14 +0530

    change 5

--- backend/services/career_pipeline.py
+++ backend/services/career_pipeline.py
@@ -1,8 +1,8 @@
 from src.resume_parser.resume_parser import ResumeParser
 from src.resume_parser.resume_cleaner import ResumeCleaner
-from src.resume_parser.resume_embedding import ResumeEmbedding
+# from src.resume_parser.resume_embedding import ResumeEmbedding
 
-# from src.matching.embedding_search import EmbeddingSearch
+from src.matching.embedding_search import EmbeddingSearch
 from src.matching.skill_gap_analyzer import SkillGapAnalyzer
 
 from src.career_advisor.career_advisor import CareerAdvisor
