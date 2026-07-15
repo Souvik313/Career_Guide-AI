aec8a282a38a5c73a040454915c950d41692f1f0
Souvik313 <roysouvik8220@gmail.com>
2026-07-16 00:33:19 +0530

    Modified some files

--- src/matching/embedding_search.py
+++ src/matching/embedding_search.py
@@ -12,52 +12,8 @@ from src.config import (
 class EmbeddingSearch:
 
     def __init__(self):
-        self.model = SentenceTransformer(EMBEDDING_MODEL)
         self.index = faiss.read_index(str(FAISS_INDEX_PATH))
         self.jobs = pd.read_csv(PROCESSED_DATA_PATH_WITH_SKILLS)
-
-    def embed_query(self , query):
-        """
-        Takes a query and converts it into a normalized embedding
-        """
-
-        embedding = self.model.encode(
-            [query],
-            convert_to_numpy = True
-        )
-
-        embedding = embedding.astype(np.float32)
-        faiss.normalize_L2(embedding)
-
-        return embedding
-    
-    def _retrieve_results(self , scores , indices):
-        """
-        Convert FAISS search ourput into a dataframe
-        """
-
-        results = self.jobs.iloc[indices[0]].copy()
-        results["Similarity Score"] = scores[0]
-        results = results.sort_values(
-            by="Similarity Score",
-            ascending=False
-        ).reset_index(drop=True)
-
-        return results
-    
-    def search(self , query , top_k = 10):
-        """
-        Search for the most similar jobs
-        """
-
-        query_embedding = self.embed_query(query)
-        scores, indices = self.index.search(
-            query_embedding,
-            top_k
-        )
-        return self._retrieve_results(
-            scores , indices
-        )
     
     def search_by_embedding(self , embedding , top_k = 10):
         """
--- src/utils/download_models.py
+++ src/utils/download_models.py
@@ -12,3 +12,19 @@ def download_file(filename):
         filename=filename,
         repo_type="dataset",
     )
+
+def get_model_paths():
+
+    embedding_path = hf_hub_download(
+        repo_id=REPO_ID,
+        filename="job_embeddings.npy",
+        repo_type="dataset",
+    )
+
+    faiss_path = hf_hub_download(
+        repo_id=REPO_ID,
+        filename="faiss_index.index",
+        repo_type="dataset",
+    )
+
+    return embedding_path, faiss_path
