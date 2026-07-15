a0ca241d1922d275e6b55e0dbffa7fd46ad4c0eb
Souvik313 <roysouvik8220@gmail.com>
2026-07-15 15:20:00 +0530

    Modification

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,18 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from backend.routes.resume_routes import router as resume_router
 
 app = FastAPI(
-    title="CareerAdvisor AI API",
-    description="AI powered Resume analysis and career recommendation API",
-    version="1.0.0",
+    title="CareerCompass AI API",
+    version="1.0.0"
 )
 
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-)
-
-# Include API routes
-app.include_router(resume_router)
-
-# Endpoints
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to CareerCompass-AI API",
-        "status": "Running"
+        "status": "running"
     }
 
 @app.get("/health")
-def health_check():
-    return{
-        "status": "healthy"
+def health():
+    return {
+        "healthy": True
     }
