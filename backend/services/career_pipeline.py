cb9aaa84b65d6537fbbb546e4842e63e9a5d6b18
Souvik313 <roysouvik8220@gmail.com>
2026-07-15 16:20:24 +0530

    change 3

--- backend/services/career_pipeline.py
+++ backend/services/career_pipeline.py
@@ -1,13 +1,13 @@
-# from src.resume_parser.resume_parser import ResumeParser
-# from src.resume_parser.resume_cleaner import ResumeCleaner
+from src.resume_parser.resume_parser import ResumeParser
+from src.resume_parser.resume_cleaner import ResumeCleaner
 # from src.resume_parser.resume_embedding import ResumeEmbedding
 
 # from src.matching.embedding_search import EmbeddingSearch
-# from src.matching.skill_gap_analyzer import SkillGapAnalyzer
+from src.matching.skill_gap_analyzer import SkillGapAnalyzer
 
-# from src.career_advisor.career_advisor import CareerAdvisor
+from src.career_advisor.career_advisor import CareerAdvisor
 
-# from src.llm.career_report_generator import CareerReportGenerator
+from src.llm.career_report_generator import CareerReportGenerator
 
 class CareerPipeline:
     def run_pipeline(self, resume_path):
